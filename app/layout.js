b59b6fc0a47ad4154311ff308c61b26abb43c97a
Arshal-Agarwal <ajagarwal2005@gmail.com>
2024-09-10 16:06:36 +0530

    Project necessities

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,8 @@
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,10 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+
+        {/* Main container with flex-grow to take up remaining space */}
+        <div className="flex-grow container mx-auto px-4">
+          {children}
+        </div>
+
+        <Footer />
       </body>
     </html>
   );
