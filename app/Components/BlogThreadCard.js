7d7b69d2d0020c272517d139a70c02fb7f9e1243
Arshal-Agarwal <ajagarwal2005@gmail.com>
2024-09-14 00:55:25 +0530

    Fixed npm issues
    1) Fixed issues caused due to HTML JSX overlap and mutually exclusive keywords

--- app/Components/BlogThreadCard.js
+++ app/Components/BlogThreadCard.js
@@ -11,21 +11,21 @@ export default function BlogContentCard() {
                 <div className="flex items-center justify-between">
                 <Image className="hidden object-cover w-10 h-10 mr-4 rounded-full sm:block" src="/logo.png" height={100} width={100} alt="avatar"/>
                     <span className="text-sm font-light text-gray-600 dark:text-gray-400">u/Arshal11</span>
-                    <p className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-default hover:bg-gray-500" tabindex="0" role="button">Design</p>
+                    <p className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-300 transform bg-gray-600 rounded cursor-default hover:bg-gray-500" tabIndex="0" role="button">Design</p>
                 </div>
 
                 <div className="mt-2">
-                    <Link href="../Pages/DisplayBlog" className="text-xl underline font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline" tabindex="0" role="link">Blog Title</Link>
+                    <Link href="../Pages/DisplayBlog" className="text-xl underline font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline" tabIndex="0" role="link">Blog Title</Link>
                     <p className="mt-2 text-gray-600 dark:text-gray-300">Blog Content.....Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
                 </div>
                 
 
                 <div className="flex items-center justify-between mt-4">
-                    <Link href="../Pages/DisplayBlog" className="text-blue-600 dark:text-blue-400 hover:underline" tabindex="0" role="link">Read more</Link>
+                    <Link href="../Pages/DisplayBlog" className="text-blue-600 dark:text-blue-400 hover:underline" tabIndex="0" role="link">Read more</Link>
 
                     <div className="flex items-center">
                         
-                            <Link className="font-bold text-gray-700 cursor-pointer dark:text-gray-200" tabindex="0" role="link"href="../Pages/DisplayBlog">Arshal Agarwal</Link>
+                            <Link className="font-bold text-gray-700 cursor-pointer dark:text-gray-200" tabIndex="0" role="link"href="../Pages/DisplayBlog">Arshal Agarwal</Link>
                     </div>
                 </div>
             </div>
