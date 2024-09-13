bf70b7a61eace082a95a6f12f21df11f7cb756a6
Arshal-Agarwal <ajagarwal2005@gmail.com>
2024-09-13 17:49:30 +0530

    Major changes to Compose page
    1) Removed all conditionally rendered elements and tags.
    2) replaced and restyled the tags and category input before blog content
    3) Integrated all submit buttons into 1 button so that all field inputs are passed by a singular button.
    4) Removed Image tag

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
                         <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
                             <Link href="/" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Home</Link>
                             <Link href="/Pages/Compose" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Compose Blog</Link>
-                            <Link href="/" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Your Blogs</Link>
+                            <Link href="/" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">My Profile</Link>
                             <Link href="/Pages/SignIn" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Sign in</Link>
                         </div>
 
--- app/Pages/Compose/page.js
+++ app/Pages/Compose/page.js
@@ -23,9 +23,15 @@ export default function Compose() {
 
     function submitClick() {
         let Blog_Content = blog_ref.current.value;               //When connecting backend , add this Blog_Content in db
-        let Blog_Title = title_ref.current.value;                //When connecting backend , add this Blog_title in db
+        let Blog_Title = title_ref.current.value;  
+        let Blog_tags= tag_ref.current.value;
+        let Blog_category= cat_ref.current.value;
+
         console.log("Blog title " + Blog_Title);
         console.log("Blog content : " + Blog_Content);
+        console.log("Blog tags : " + Blog_tags);
+        console.log("Blog category : " + Blog_category);
+
         setshowAlert(true);
         setTimeout(() => {
             setshowAlert(false);
@@ -38,44 +44,12 @@ export default function Compose() {
         setblogContent("");
     }
 
-    function onTagClick() {
-        setTimeout(() => {
-            tag_ref.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-        }, 0);
-        setshowTagInput(!showTagInput);
-
-    }
-
-    function onCategoryClick() {
-        
-        setTimeout(() => {
-            
-            cat_ref.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-
-        } , 0);
-        
-        
-        setshowCategoryInput(!showCategoryInput);
-
-    }
-
-
     function blogChange(e) {
         setblogContent(e.target.value)
     }
 
     function titleChange(ev) {
         setTitle(ev.target.value);
-    }
-
-    function onSubmitTagClick() {
-
-        setshowTagInput(false);
-    }
-
-    function onSubmitCategoryClick() {
-
-        setshowCategoryInput(false); 
     }
 
     function tagsChange(e1) {
@@ -90,14 +64,22 @@ export default function Compose() {
 
     return (
         <div className=" min-w-64  pl-24 mt-4">
-            
+
             <label className='username text-xs' htmlFor="">User : u/Arshal11</label>
-            <input className="title w-3/4 p-2  border border-gray-300 rounded mt-2 block" ref={title_ref} type="text" value={Title} onChange={titleChange} placeholder='Input Title here' />
+            <input className="title w-3/4 p-2  border border-gray-300 rounded mt-2 block" ref={title_ref} type="text" value={Title} onChange={titleChange} placeholder='Enter Title here' />
+
+                
+                <input className="tags w-1/4 p-2  border border-gray-300 rounded mt-10" ref={tag_ref} type="text" placeholder='Enter Tags' value={Tags} onChange={(e) => tagsChange(e)} />
+            
+
+            
+                <input className="tags w-1/4 p-2 ml-12  border border-gray-300 rounded mt-10" ref={cat_ref} type="text" placeholder='Enter Category' value={Category} onChange={(e) => catsChange(e)} /> 
+            
 
             <div className='pr-16 mt-8'>
-                <label htmlFor="Description" className="block text-xs text-gray-500 dark:text-gray-300">Tags:  </label>
+                {/* <label htmlFor="Description" className="block text-xs text-gray-500 dark:text-gray-300">Tags:  </label> */}
 
-                <textarea ref={blog_ref} id="content" onChange={(e) => blogChange(e)} value={blogContent} placeholder="Enter blog content" className="block  mt-2  w-full  rounded-lg border border-gray-200 bg-white px-4 h-96 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600  dark:text-gray-300 dark:focus:border-blue-300"></textarea>
+                <textarea ref={blog_ref} id="content" onChange={(e) => blogChange(e)} value={blogContent} placeholder="Enter blog content" className="block  mt-2  w-full  rounded-lg border border-gray-200 bg-white px-2 h-96 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600  dark:text-gray-300 dark:focus:border-blue-300"></textarea>
             </div>
 
             <button onClick={submitClick} className="mt-6  px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
@@ -106,44 +88,21 @@ export default function Compose() {
                 {/* </Link> */}
             </button>
 
-            <button onClick={submitClick} className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" >
-                Draft
-            </button>
+           
 
             <button onClick={onClearClick} className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
                 Clear
             </button>
 
-            <button className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" onClick={onTagClick}>
-                Add Tags
-            </button>
-
-            <button className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" onClick={onCategoryClick}>
-                Category
-            </button>
-
-            <button className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" >
-                Image
-            </button>
-
-            {showTagInput && <div className="tags">
-                <input className="tags w-3/4 p-2  border border-gray-300 rounded mt-10" ref={tag_ref} type="text" placeholder='Input Tags here , Separate them with a space' value={Tags} onChange={(e) => tagsChange(e)} />
-                <button className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" onClick={onSubmitTagClick}>
-                    Submit Tags
-                </button>
-            </div>
-            }
-
-            {showCategoryInput && <div className="category">
-                <input className="tags w-3/4 p-2  border border-gray-300 rounded mt-10" ref={cat_ref} type="text" placeholder='Input Tags here , Separate them with a space' value={Category} onChange={(e) => catsChange(e)} />
-                <button className="px-6 ml-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-900 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" onClick={onSubmitCategoryClick}>
-                    Submit Category
-                </button>
-            </div>
-            }
+            
 
 
-            {showAlert && <div className="flex ml-auto mr-16  max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+
+
+
+
+
+            {showAlert && <div className="flex float-right ml-auto mr-16  max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
                 <div className="flex items-center justify-center w-12 bg-emerald-500">
                     <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
                         <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z" />
@@ -157,7 +116,7 @@ export default function Compose() {
                     </div>
                 </div>
             </div>}
-            
+
         </div>
 
     )
